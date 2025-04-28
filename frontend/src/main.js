17ee2df66ea2619c4cec8190b878af17d36e130a
mgalihp10 <61969338+mgalihp10@users.noreply.github.com>
2025-04-29 04:14:08 +0700

    Add frontend/src/main.js 1745874847929

---
+++ frontend/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(BootstrapVue3)
+app.mount('#app')
